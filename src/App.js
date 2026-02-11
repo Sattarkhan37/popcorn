aafc944ff3d7660b0b065cb7e0e51587f4a2f663
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-02-11 20:46:52 +0530

    fixed PropDrilling with composition for ListBox

--- src/App.js
+++ src/App.js
@@ -53,19 +53,24 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} ><Logo />
+      <NavBar movies={movies}>
+        <Logo />
         <Search />
-        <NumResults movies={movies} /></NavBar>
-      <Main movies={movies} />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 function NavBar({ children }) {
   return (
     <>
-      <nav className="nav-bar">
-        {children}
-      </nav>
+      <nav className="nav-bar">{children}</nav>
     </>
   );
 }
@@ -96,16 +101,11 @@ function NumResults({ movies }) {
     </p>
   );
 }
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -115,7 +115,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
